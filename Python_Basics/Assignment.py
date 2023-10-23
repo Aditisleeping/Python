60b9f6d8e6bc033440168b3f534c54dda915bed7
Aditi Seshadri <aditi.seshadri@gmail.com>
2023-10-23 16:46:01 +0530

    Adding an Assignment program on Multiple Assignment

---
+++ Python_Basics/Assignment.py
@@ -0,0 +1,24 @@
+#Multiple assignment
+x=y=z=50
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
+#Changing x
+x = 500
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
+#Changing x and y
+x = y = 200
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
